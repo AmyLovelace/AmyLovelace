403e2a5a35b9c11712231b95f954df5efb04fc9d
AmyLovelace <amanecer.luz.cabrera@gmail.com>
2025-05-21 06:47:16 +0000

    generated 2025-05-21

--- quotes.js
+++ quotes.js
@@ -1,4 +1,5 @@
 const quotes = [
-  "Two years ago we had 50 million people subscribed to music streaming services around the world. Today we have 400 million."
+  "Two years ago we had 50 million people subscribed to music streaming services around the world. Today we have 400 million.",
+  "I spoke to Dave Chapelle for two hours this morning. He is our modern day Socrates"
 ];
 module.exports = quotes;
