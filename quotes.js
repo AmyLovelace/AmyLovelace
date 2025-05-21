cae304418b904e8e9b08b2a0c7fa048c4fe37146
AmyLovelace <amanecer.luz.cabrera@gmail.com>
2025-05-21 09:29:28 +0000

    generated 2025-05-21

--- quotes.js
+++ quotes.js
@@ -7,6 +7,7 @@ const quotes = [
   "I feel like me and Taylor might still have sex",
   "We have to evolve",
   "I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle",
-  "I feel like I'm too busy writing history to read it."
+  "I feel like I'm too busy writing history to read it.",
+  "I care. I care about everything. Sometimes not giving a f#%k is caring the most."
 ];
 module.exports = quotes;
